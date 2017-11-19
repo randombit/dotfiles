225cba58cfa8971b1773c9d36da2654638f7a50a
Jack Lloyd <jack@randombit.net>
2017-11-19 13:19:58 -0500

    Save some things

--- emacs/jl-indent.el
+++ emacs/jl-indent.el
@@ -89,51 +89,7 @@
       )
 ))
 
-(setq broadway-style '(
-   (c-basic-offset . 2)
-   (c-comment-only-line-offset . 0)
-   (c-offsets-alist
-      (c . 0)
-      (comment-intro . 0)
-
-      (statement-block-intro . +)
-      (statement-cont . +)
-
-      (substatement . +)
-      (substatement-open . 0)
-
-      (block-open . 0)
-      (block-close . 0)
-
-      (defun-open . 0)
-      (defun-close . 0)
-      (defun-block-intro . +)
-      (func-decl-cont . +)
-
-      (case-label . 2)
-
-      (class-open . 0)
-      (class-close . 0)
-      (inclass . +)
-      (access-label . -2)
-      (inline-open . 0)
-      (inline-close . 0)
-
-      (extern-lang-open . 0)
-      (extern-lang-close . 0)
-      (inextern-lang . 0)
-
-      (statement-case-open . +)
-
-      (namespace-open . 0)
-      (namespace-close . 0)
-      (innamespace . 0)
-
-      (label . 0)
-      )
-))
-
 (defun set_styles()
   (c-add-style "botan" botan-style t)
-  (c-add-style "randombit" randombit-style nil)
-  (c-add-style "broadway" broadway-style nil))
+  (c-add-style "randombit" randombit-style nil))
+
